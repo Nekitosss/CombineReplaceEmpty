6bea30afcc91f88ce92c88ace8c34e4ae147cf46
Nikita Patskov <patskovn@yahoo.com>
2019-12-24 11:41:20 +0300

    Remove replace Failure (.catch exists)

--- Tests/CombineReplaceWithPublisherTests/CombineReplaceWithPublisherTests.swift
+++ Tests/CombineReplaceWithPublisherTests/CombineReplaceWithPublisherTests.swift
@@ -44,46 +44,6 @@ final class CombineReplaceWithPublisherTests: XCTestCase {
         XCTAssertEqual(result, "5")
     }
     
-    func testReplaceErrorHandling() {
-        var cancellable = Set<AnyCancellable>()
-        var result: String?
-        
-        Result<String, Error>.Publisher(.failure(NSError(domain: "Internal", code: 0, userInfo: nil)))
-            .replaceEmpty(Result.Publisher("5"))
-            .replaceError(Just("4"))
-            .sink(receiveValue: { result = $0 })
-            .store(in: &cancellable)
-        
-        XCTAssertEqual(result, "4")
-    }
-    
-    func testReplaceErrorNotHandling() {
-        
-        var cancellable = Set<AnyCancellable>()
-        var result: String?
-        
-        Result<String, Error>.Publisher(.success("3"))
-            .replaceError(Just("4"))
-            .sink(receiveValue: { result = $0 })
-            .store(in: &cancellable)
-        
-        XCTAssertEqual(result, "3")
-    }
-    
-    func testReplaceErrorChain() {
-        
-        var cancellable = Set<AnyCancellable>()
-        var result: String?
-        
-        Result<String, Error>.Publisher(.failure(NSError(domain: "Internal", code: 0, userInfo: nil)))
-            .replaceError(Result.Publisher(.failure(NSError(domain: "Another", code: 0, userInfo: nil))))
-            .replaceError(Just("4"))
-            .sink(receiveValue: { result = $0 })
-            .store(in: &cancellable)
-        
-        XCTAssertEqual(result, "4")
-    }
-    
     static var allTests = [
         ("testReplaceEmptyHandling", testReplaceEmptyHandling),
     ]
